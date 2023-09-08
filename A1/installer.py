4c9e3f20f5a5d42b1d6fe1bd94c855c8704ccc35
Lachlan Morrow <s3942121@student.rmit.edu.au>
2023-09-08 13:46:21 +1000

    Almost completed pass requirements

--- A1/installer.py
+++ A1/installer.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python
+from datetime import datetime
 import os
-file = open("information.txt", "w")
+import webbrowser
+
+tFile = open("information.txt", "w")
+
+# write System Time
+tFile.write("System Time: "+str(datetime.now()) + "\n")
+
+# write student numbers
+tFile.write("Student 1 (Manav) Number: s3949664 \nStudent 2 (Lachlan) Number: s3942121\n")
+
+# Open URL (google)
+# webbrowser.open("http://www.google.com")
+
+# Write username
+tFile.writelines("\nCurrent User: "+ str(os.getlogin()))
+
+
+# Close text file
+tFile.close()
+
+devInfoFile = open("DeviceInfos3949664s3942121.dll", "w")
 
 for a in os.environ:
-    file.writelines(a + '=' + os.getenv(a))
-    file.writelines("\n")
-    if a == os.getenv('USER'):
-        username = a
+    devInfoFile.writelines(a+ '='+ os.getenv(a)+"\n")
+    if(str(a) == "HOMEDRIVE"):
+        homedrive = a
 
-file.writelines("\n")
-file.writelines(a)
-file.close()
+
+
+devInfoFile.close()
